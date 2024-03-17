c3cc666c4211f0c32c57228392604d9754923e97
veron <veron@LAPTOP-M6AORV88>
2024-03-17 22:40:53 +0100

    Nueva rama

---
+++ Proyecto1Git/src/Nuevopaquete/RamaNueva.java
@@ -0,0 +1,10 @@
+package Nuevopaquete;
+
+public class RamaNueva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("creo una nueva rama");
+	}
+
+}
